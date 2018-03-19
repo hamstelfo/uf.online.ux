c9b64f33c800364481343462f92166241999b9a8
hamstelfo <hamstelfo@gmail.com>
2018-03-19 16:50:27 +0100

    images.ts instead .js
    Signed-off-by: hamstelfo <hamstelfo@gmail.com>

---
+++ UFRole/src/components/images.ts
@@ -0,0 +1,8 @@
+import circle from '../images/circle.svg';
+import circleYellow from '../images/circle.yellow.svg';
+import field from '../images/field.svg';
+export default {
+    circle,
+    circleYellow,
+    field
+}
